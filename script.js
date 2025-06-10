1ba346776f8bc63392aa4138d7067fd1c1bed0c7
Joshua Concepcion <joshuarconcepcion@gmail.com>
2025-06-10 14:39:38 -0700

    Basic js for the add book modal, css styling for books, fonts, and colors.

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 const myLibrary = [];
 
-function Book(title, author, pages, status) {
+function Book(title, author, status) {
     if (!(this instanceof Book)) {
         throw new Error("Use new operator");
     }
 
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.status = status;
 }
 
-function addBook(title, author, pages, status) {
+function addBook(title, author, status) {
     let book = new Book(title, author, pages, status);
 
     myLibrary.push(book);
 }
+
+const addBtn = document.querySelector('#add-book');
+const addModal = document.querySelector('.add-modal');
+const libraryView = document.querySelector('.library-cards');
+
+addBtn.addEventListener("click", () => {
+    addModal.classList.remove('hidden');
+    libraryView.classList.add('blur');
+});
+
+const submitBtn = document.querySelector('#submit-btn');
+
+submitBtn.addEventListener("click", () => {
+    addModal.classList.add('hidden');
+    libraryView.classList.remove('blur');
+});
